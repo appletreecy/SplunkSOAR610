fbcb6e522d4e44ed7eff61abdf7e2e13101bd1eb
soar_local_admin <root@localhost>
2023-08-03 04:54:44 +0000

    1

---
+++ scao_container_test.py
@@ -0,0 +1,68 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+@phantom.playbook_block()
+def on_start(container):
+    phantom.debug('on_start() called')
+
+    # call 'container_update_1' block
+    container_update_1(container=container)
+
+    return
+
+@phantom.playbook_block()
+def container_update_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("container_update_1() called")
+
+    parameters = []
+
+    parameters.append({
+        "container_input": None,
+        "name": None,
+        "description": None,
+        "label": None,
+        "owner": None,
+        "sensitivity": None,
+        "severity": None,
+        "status": "closed",
+        "tags": None,
+        "input_json": None,
+    })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.custom_function(custom_function="community/container_update", parameters=parameters, name="container_update_1")
+
+    return
+
+
+@phantom.playbook_block()
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
