9101e9f7eaaacf03bbc54e28e8e3acf68d4d224b
soar_local_admin <root@localhost>
2023-08-16 01:36:55 +0000

    1

--- scao_test_collect.py
+++ scao_test_collect.py
@@ -26,7 +26,7 @@ def code_1(action=None, success=None, container=None, results=None, handle=None,
     ################################################################################
 
     # Write your custom code here...
-    data = phantom.collect(container, "artifact:*.cef.destinationUserName[0]")
+    data = phantom.collect(container, "artifact:*")
     
     phantom.debug(data)
 
