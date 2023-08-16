6d667177b824d00b75cd5842f09e8981d8880345
soar_local_admin <root@localhost>
2023-08-16 01:42:50 +0000

    1

---
+++ scao_add_artifact.py
@@ -0,0 +1,60 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+@phantom.playbook_block()
+def on_start(container):
+    phantom.debug('on_start() called')
+
+    # call 'code_1' block
+    code_1(container=container)
+
+    return
+
+@phantom.playbook_block()
+def code_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("code_1() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+    raw = {}
+    cef = {}
+    cef['sourceAddress'] = '1.1.1.1'
+
+    success, message, artifact_id = phantom.add_artifact(
+        container=container, raw_data=raw, cef_data=cef, label='netflow',
+        name='test_event', severity='high',
+        identifier=None,
+        artifact_type='network')
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
+
+
+@phantom.playbook_block()
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
